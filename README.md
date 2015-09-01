65e367fbe4f4309bc97dff04ecbd1767c1a62a76
Julian Nacci <julian.nacci@gmail.com>
2015-09-01 14:44:01 +0200

    modified readme and removed app controller overwrite for pundit

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# Rails Templates
+## Custom Template
 
-Quickly generate a rails app with the default [Wagon](http://www.lewagon.org) configuration
-using [Rails Templates](http://guides.rubyonrails.org/rails_application_templates.html).
-
-## Minimal
-
-Get a minimal rails app ready to be deployed on Heroku with Bootstrap, Simple form and
-debugging gems.
+Get a minimal rails app ready to be deployed on Heroku with Bootstrap, Devise, Simple form and debugging gems.
 
 ```bash
 rails new \
   -T --database postgresql \
   -m https://raw.githubusercontent.com/JulianNacci/rails-templates/master/minimal.rb \
-  CHANGE_THIS_TO_YOUR_RAILS_APP_NAME
+  CHANGE_NAME
 ```
--- minimal.rb
+++ minimal.rb
@@ -185,23 +185,6 @@ file 'app/views/shared/_flashes.html.erb', <<-HTML
 <% end %>
 HTML
 
-file 'app/controllers/application_controller.rb', <<-RUBY
-class ApplicationController < ActionController::Base
-
-  before_action :authenticate_user!
-  include Pundit
-
-  after_action :verify_authorized, except: :index, unless: :devise_controller?
-  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(root_path)
-  end
-end
-RUBY
 
 file 'config/initializers/geocoder.rb', <<-RUBY
 Geocoder.configure(
